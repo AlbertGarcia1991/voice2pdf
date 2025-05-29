b69818026343c88c44ae34fa4c4531673b6d77d0
AlbertGarciaPlaza <albert.plaza@lego.com>
2025-05-29 11:33:15 +0100

    Added eslint to frontend github actions

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,18 +1,22 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
+    es2021: true,
     node: true,
-    'vue/setup-compiler-macros': true
   },
   extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended'
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2021,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'vue/multi-word-component-names': 'off',
+  },
 } 
